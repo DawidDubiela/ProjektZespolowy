b56d1415bdbb76659d1ea2e863a85819be1535b0
Manji <xmadziej@gmail.com>
2020-11-30 02:06:10 +0100

    sprawdzanie konsultantow z bazy

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql")
-const express = require("express");
+const express = require("express")
 const bodyParser = require("body-parser")
 const encoder = bodyParser.urlencoded()
 
@@ -8,47 +8,46 @@ app.use(express.static("public"))
 app.use(express.json())
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Maciej12',
-    database: 'project'
+  host: 'localhost',
+  user: 'root',
+  password: 'Maciej12',
+  database: 'project'
 })
 
 
 //laczymy sie z baza danych
-connection.connect(function(error)
-{
-    if(error) throw error
-    else console.log("Connected to the database sucessfully!")
+connection.connect(function (error) {
+  if (error) throw error
+  else console.log("Connected to the database sucessfully!")
 })
 
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html")
 })
 
-app.post("/",encoder, function(req,res){
-  var username = req.body.username;
-  var password = req.body.password;
+app.post("/", encoder, function (req, res) {
+  var username = req.body.username
+  var password = req.body.password
 
 
-connection.query("select * from student where email = ? and password = ?",[username,password],
-function(error,results,fields){
-  console.log(error)
-  if(results.length > 0)
-  {
-    res.redirect("/kalendarz");
-  }
-  else
-  {
-    res.redirect("/");
-  }
-  res.end();
-})
- })
-
-app.get("/kalendarz", function(req,res)
-{
-  res.sendFile(__dirname + "/kalendarz.html")
+  connection.query("select * from student where email = ? and password = ? ", [username, password],
+    function (error, results, fields) {
+      if (results.length > 0) {
+        res.sendFile(__dirname + "/kalendarz.html")
+      }
+      else {
+        connection.query("select * from consultant where email = ? and password = ? ", [username, password],
+          function (error, results, fields) {
+            if (results.length > 0) {
+              res.sendFile(__dirname + "/kalendarz.html")
+            }
+            else {
+              res.redirect("/")
+            }
+            res.end()
+          })
+      }
+    })
 })
 
 //ustawiamy port
